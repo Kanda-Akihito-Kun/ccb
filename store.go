a9128264572b82eb39b88c398c63576ee95c5467
鼠鼠今天吃嘉然 <kandaakihitokun@gmail.com>
2025-02-21 17:44:44 +0800

    fix - 地区列表有空值

--- store.go
+++ store.go
@@ -2,16 +2,20 @@ package main
 
 var (
 	regionPatternMap = map[string]string{
-		"-sz-":   "深圳",
-		"-hz-":   "杭州",
-		"-hk-":   "香港",
-		"-sh-":   "上海",
-		"-bj-":   "北京",
-		"-sccd-": "成都",
-		"-xj-":   "新疆",
+		"-sz-":      "深圳",
+		"-hz-":      "杭州",
+		"-hk-":      "香港",
+		"-sh-":      "上海",
+		"-bj-":      "北京",
+		"-sccd-":    "成都",
+		"-fjqz-":    "泉州",
+		"-hbwh-":    "武汉",
+		"-jsnj-":    "南京",
+		"-hljheb-":  "哈尔滨",
+		"-nmghhht-": "呼和浩特",
 	}
 
-	regionList = make([]string, len(regionPatternMap))
+	regionList = make([]string, 0, len(regionPatternMap))
 
 	// region : [url]
 	cdnMap = make(map[string][]string)
